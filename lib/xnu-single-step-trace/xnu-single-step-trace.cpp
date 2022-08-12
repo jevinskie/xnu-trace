f6749b3f4cc7aec68cbdcc8f63586459c3f2443b
Jevin Sweval <jevinsweval@gmail.com>
2022-08-12 12:02:41 -0400

    clang-format pre-commit

--- lib/xnu-single-step-trace/xnu-single-step-trace.cpp
+++ lib/xnu-single-step-trace/xnu-single-step-trace.cpp
@@ -79,7 +79,8 @@ extern "C" kern_return_t catch_mach_exception_raise_state_identity(
     mach_exception_data_t code, mach_msg_type_number_t code_count, int *flavor,
     thread_state_t old_state, mach_msg_type_number_t old_state_count, thread_state_t new_state,
     mach_msg_type_number_t *new_state_count) {
-#pragma unused(exception_port, thread, task, exception, code, code_count, flavor, old_state_count, new_state, new_state_count)
+#pragma unused(exception_port, thread, task, exception, code, code_count, flavor, old_state_count, \
+               new_state, new_state_count)
 
     auto os = (const arm_thread_state64_t *)old_state;
     // auto ns = (arm_thread_state64_t *)new_state;
