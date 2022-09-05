d2b616d0a79f788621455f1c7bfbdfe336bcbc60
Jevin Sweval <jevinsweval@gmail.com>
2022-09-05 10:06:04 -0400

    stalker freezing

--- lib/xnu-single-step-trace/FridaStalker.cpp
+++ lib/xnu-single-step-trace/FridaStalker.cpp
@@ -1,5 +1,48 @@
 #include "common.h"
 
+#define GUM_TYPE_TRACER_STALKER_TRANSFORMER (gum_tracer_stalker_transformer_get_type())
+GUM_DECLARE_FINAL_TYPE(GumTracerStalkerTransformer, gum_tracer_stalker_transformer, GUM,
+                       TRACER_STALKER_TRANSFORMER, GObject)
+
+struct _GumTracerStalkerTransformer {
+    GObject parent;
+};
+
+static void tracer_cb(GumCpuContext *context, gpointer user_data) {
+    // fmt::print(".");
+    // printf(".");
+    // __builtin_debugtrap();
+}
+
+static void gum_tracer_stalker_transformer_transform_block(GumStalkerTransformer *transformer,
+                                                           GumStalkerIterator *iterator,
+                                                           GumStalkerOutput *output) {
+    while (gum_stalker_iterator_next(iterator, nullptr)) {
+        // fmt::print(".");
+        gum_stalker_iterator_put_callout(iterator, tracer_cb, nullptr, nullptr);
+        gum_stalker_iterator_keep(iterator);
+    }
+}
+
+static void gum_tracer_stalker_transformer_iface_init(gpointer g_iface, gpointer iface_data) {
+    auto *iface = (GumStalkerTransformerInterface *)g_iface;
+
+    iface->transform_block = gum_tracer_stalker_transformer_transform_block;
+}
+
+G_DEFINE_TYPE_EXTENDED(GumTracerStalkerTransformer, gum_tracer_stalker_transformer, G_TYPE_OBJECT,
+                       0,
+                       G_IMPLEMENT_INTERFACE(GUM_TYPE_STALKER_TRANSFORMER,
+                                             gum_tracer_stalker_transformer_iface_init))
+
+static void gum_tracer_stalker_transformer_class_init(GumTracerStalkerTransformerClass *klass) {}
+
+static void gum_tracer_stalker_transformer_init(GumTracerStalkerTransformer *self) {}
+
+static GumStalkerTransformer *gum_stalker_transformer_make_tracer(void) {
+    return (GumStalkerTransformer *)g_object_new(GUM_TYPE_TRACER_STALKER_TRANSFORMER, nullptr);
+}
+
 FridaStalker::FridaStalker() {
     gum_init_embedded();
     assert(gum_stalker_is_supported());
@@ -13,11 +56,15 @@ FridaStalker::~FridaStalker() {
 }
 
 void FridaStalker::follow() {
-    gum_stalker_follow_me(m_stalker, nullptr, nullptr);
+    auto *trace_transformer = gum_stalker_transformer_make_tracer();
+    assert(trace_transformer);
+    gum_stalker_follow_me(m_stalker, trace_transformer, nullptr);
 }
 
 void FridaStalker::follow(GumThreadId thread_id) {
-    gum_stalker_follow(m_stalker, thread_id, nullptr, nullptr);
+    auto *trace_transformer = gum_stalker_transformer_make_tracer();
+    assert(trace_transformer);
+    gum_stalker_follow(m_stalker, thread_id, trace_transformer, nullptr);
 }
 
 void FridaStalker::unfollow() {
