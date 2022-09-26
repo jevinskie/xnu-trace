0bfecad90b1cb4c9d44c1de429e74c754d8633b9
Jevin Sweval <jevinsweval@gmail.com>
2022-09-25 21:34:11 -0400

    bad clang-format, bad

--- include/xnu-trace/log_structs.h
+++ include/xnu-trace/log_structs.h
@@ -2,80 +2,27 @@
 
 #include "common.h"
 
+// clang-format off
 enum class gpr_idx : uint8_t {
-    x0,
-    x1,
-    x2,
-    x3,
-    x4,
-    x5,
-    x6,
-    x7,
-    x8,
-    x9,
-    x10,
-    x11,
-    x12,
-    x13,
-    x14,
-    x15,
-    x16,
-    x17,
-    x18,
-    x19,
-    x20,
-    x21,
-    x22,
-    x23,
-    x24,
-    x25,
-    x26,
-    x27,
-    x28,
-    fp /* x29 */,
-    lr /* x30 */,
-    sp /* x31 */,
-    pc // pc = "x32" 34th
+    x0, x1, x2, x3, x4, x5, x6, x7,
+    x8, x9, x10, x11, x12, x13, x14, x15,
+    x16, x17, x18, x19, x20, x21, x22, x23,
+    x24, x25, x26, x27, x28, fp /* x29 */, lr /* x30 */, sp /* x31 */
 };
+// clang-format on
 
-constexpr uint8_t gpr_idx_sz = (uint8_t)gpr_idx::pc + 1; // pc = 33, sz = 34
+constexpr uint8_t gpr_idx_sz = (uint8_t)gpr_idx::sp + 1; // sz = 32
 
+// clang-format off
 enum class vec_idx : uint8_t {
-    v0 = 0,
-    v1,
-    v2,
-    v3,
-    v4,
-    v5,
-    v6,
-    v7,
-    v8,
-    v9,
-    v10,
-    v11,
-    v12,
-    v13,
-    v14,
-    v15,
-    v16,
-    v17,
-    v18,
-    v19,
-    v20,
-    v21,
-    v22,
-    v23,
-    v24,
-    v25,
-    v26,
-    v27,
-    v28,
-    v29,
-    v30,
-    v31 // v31 32nd
+    v0 = 0, v1, v2, v3, v4, v5, v6, v7,
+    v8, v9, v10, v11, v12, v13, v14, v15,
+    v16, v17, v18, v19, v20, v21, v22, v23,
+    v24, v25, v26, v27, v28, v29, v30, v31 // v31 32nd
 };
+// clang-format on
 
-constexpr uint8_t vec_idx_sz = (uint8_t)vec_idx::v31 + 1; // v31 = 31, s = 32
+constexpr uint8_t vec_idx_sz = (uint8_t)vec_idx::v31 + 1; // sz = 32
 
 struct log_msg_hdr {
     uint32_t gpr_changed;
