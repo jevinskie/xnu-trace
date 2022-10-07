a7d18e7634551114b294f7347d1fe616d0a8ec7e
Jevin Sweval <jevinsweval@gmail.com>
2022-10-07 15:31:43 -0400

    broken wip

--- include/xnu-trace/BitVector.h
+++ include/xnu-trace/BitVector.h
@@ -5,11 +5,46 @@
 #include "Atomic.h"
 #include "utils.h"
 
+#include <memory>
 #include <type_traits>
 
 #include <icecream.hpp>
 
+template <uint8_t NBits, bool Signed = false> class ExactBitVector;
+
+template <uint8_t NBitsMax, bool Signed = false> class BitVectorBase {
+public:
+    static_assert_cond(NBitsMax > 0 && NBitsMax <= 32);
+    using T = int_n<NBitsMax, Signed>;
+    virtual T get(size_t idx);
+    virtual T set(size_t idx, T val);
+    virtual ~BitVectorBase() {}
+
+private:
+    std::vector<uint8_t> m_buf;
+};
+
+template <uint8_t NBits, bool Signed> class ExactBitVector : public BitVectorBase<NBits, Signed> {
+public:
+    static_assert_cond(NBits >= 8 && is_pow2(NBits));
+
+private:
+};
+
 template <uint8_t NBitsMax, bool Signed = false, bool AtomicWrite = false> class BitVector {
+public:
+    static_assert_cond(NBitsMax > 0 && NBitsMax <= 32);
+    BitVector(uint8_t nbits, size_t sz) {
+        if (nbits >= 8 && is_pow2(nbits)) {
+            m_bv = std::make_unique<ExactBitVector<NBitsMax, Signed>>(sz);
+        }
+    }
+
+private:
+    std::unique_ptr<BitVectorBase<NBitsMax, Signed>> *m_bv;
+};
+
+template <uint8_t NBitsMax, bool Signed = false, bool AtomicWrite = false> class BitVectorMega {
 public:
     static_assert_cond(NBitsMax > 0 && NBitsMax <= 32);
     using T                        = int_n<NBitsMax, Signed>;
@@ -21,7 +56,7 @@ public:
     using AQT                      = std::conditional_t<AtomicWrite, std::atomic<_QT>, void>;
     static constexpr uint8_t QBits = AtomicWrite ? sizeofbits<_QT>() : 0;
 
-    BitVector(uint8_t nbits, size_t sz) : m_nbits{nbits}, m_sz{sz} {
+    BitVectorMega(uint8_t nbits, size_t sz) : m_nbits{nbits}, m_sz{sz} {
         assert(nbits > 0 && nbits <= NBitsMax);
     }
 
@@ -51,4 +86,5 @@ private:
     uint8_t m_nbits;
 };
 
-template <uint8_t NBitsMax, bool Signed> using AtomicBitVector = BitVector<NBitsMax, Signed, true>;
+template <uint8_t NBitsMax, bool Signed>
+using AtomicBitVectorMega = BitVectorMega<NBitsMax, Signed, true>;
--- unit-tests/BitVector.cpp
+++ unit-tests/BitVector.cpp
@@ -6,35 +6,35 @@
 #define TS "[BitVector]"
 
 TEST_CASE("exact_fit", TS) {
-    const auto ebv = BitVector<16, false>{16, 0};
+    const auto ebv = BitVectorMega<16, false>{16, 0};
     REQUIRE(ebv.exact_fit(16));
-    const auto ebv2 = BitVector<16, false>{8, 0};
+    const auto ebv2 = BitVectorMega<16, false>{8, 0};
     REQUIRE(ebv.exact_fit(8));
-    const auto ebv3 = BitVector<32, false>{16, 0};
+    const auto ebv3 = BitVectorMega<32, false>{16, 0};
     REQUIRE(ebv.exact_fit(16));
-    const auto nebv = BitVector<16, false>{15, 0};
+    const auto nebv = BitVectorMega<16, false>{15, 0};
     REQUIRE_FALSE(nebv.exact_fit(15));
-    const auto nebv2 = BitVector<16, false>{15, 0};
+    const auto nebv2 = BitVectorMega<16, false>{15, 0};
     REQUIRE_FALSE(nebv2.exact_fit(7));
-    const auto nebv3 = BitVector<32, false>{24, 0};
+    const auto nebv3 = BitVectorMega<32, false>{24, 0};
     REQUIRE_FALSE(nebv3.exact_fit(24));
-    const auto nebv4 = BitVector<32, false>{4, 0};
+    const auto nebv4 = BitVectorMega<32, false>{4, 0};
     REQUIRE_FALSE(nebv4.exact_fit(4));
 }
 
 TEST_CASE("buf_size", TS) {
-    const auto ebv = BitVector<16, false>{16, 0};
+    const auto ebv = BitVectorMega<16, false>{16, 0};
     REQUIRE(ebv.buf_size(16, 4) == 4);
-    const auto bv = BitVector<16, false>{15, 0};
+    const auto bv = BitVectorMega<16, false>{15, 0};
     REQUIRE(bv.buf_size(15, 3) == 3);
-    const auto abv = AtomicBitVector<16, false>{15, 0};
+    const auto abv = AtomicBitVectorMega<16, false>{15, 0};
     REQUIRE(abv.buf_size(15, 3) == 4);
 }
 
 TEST_CASE("build_bv", TS) {
-    auto bv = BitVector<32, false>{31, 243};
+    auto bv = BitVectorMega<32, false>{31, 243};
 }
 
 TEST_CASE("build_abv", TS) {
-    auto abv = AtomicBitVector<32, false>{31, 243};
+    auto abv = AtomicBitVectorMega<32, false>{31, 243};
 }
