5fb901baace11d0e7a2f3a1b8a8c57165f210d88
Jevin Sweval <jevinsweval@gmail.com>
2022-10-26 14:50:41 -0400

    Looks like identical code after optimization

--- test/iterator-proxy.cpp
+++ test/iterator-proxy.cpp
@@ -1,7 +1,42 @@
 #include "xnu-trace/BitVector.h"
 
+XNUTRACE_NOINLINE
+uint64_t psum_get(NonAtomicBitVectorImpl<17> &bv, size_t num) {
+    uint64_t sum = 0;
+    for (size_t i = 0; i < num; ++i) {
+        sum += bv.get(i);
+    }
+    return sum;
+}
+
+XNUTRACE_NOINLINE
+uint64_t psum_op(NonAtomicBitVectorImpl<17> &bv, size_t num) {
+    uint64_t sum = 0;
+    for (size_t i = 0; i < num; ++i) {
+        sum += bv[i];
+    }
+    return sum;
+}
+
+XNUTRACE_NOINLINE
+uint64_t psum_get_te(BitVector<> &bv, size_t num) {
+    uint64_t sum = 0;
+    for (size_t i = 0; i < num; ++i) {
+        sum += bv.get(i);
+    }
+    return sum;
+}
+
+XNUTRACE_NOINLINE
+uint64_t psum_op_te(BitVector<> &bv, size_t num) {
+    uint64_t sum = 0;
+    for (size_t i = 0; i < num; ++i) {
+        sum += bv[i];
+    }
+    return sum;
+}
+
 int main() {
     auto bv = NonAtomicBitVectorImpl<17>(8);
-    bv[0]   = 1;
-    return bv[0] != 1;
+    return psum_get(bv, 5) + psum_op(bv, 5) + psum_get_te(bv, 5) + psum_op_te(bv, 5);
 }
